a20b2bb50cc87b4dd4c9912223b6e16ce7fd94e2
Pontni <maromitausa@gmail.com>
2025-08-29 13:23:38 -0700

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,1 +1,5 @@
+# === BEGIN FILE: ui/__init__.py ===
+from . import intro, key_pieces, outline, synopsis, brainstorm, drafting
+__all__ = ["intro", "key_pieces", "outline", "synopsis", "brainstorm", "drafting"]
+# === END FILE ===
 
